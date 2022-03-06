f3a3c69c9b3dd8e244145cbd64caa66059da414c
Yuta <73571385+yu-bil@users.noreply.github.com>
2022-03-06 17:22:41 +0900

    Update selenium_middleware.py

--- selenium_middleware.py
+++ selenium_middleware.py
@@ -1,28 +1,32 @@
-# -*- coding: utf-8 -*-
-import os.path
+#!/usr/bin/python3
+# coding: utf-8
+import cgi
+import sys, io 
 
-from urlparse import urlparse
+# python -m http.server 8000 --cgi 
+# http://localhost:8000/cgi-bin/calc1.py
 
-import arrow
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
-from scrapy.http import HtmlResponse
-from selenium.webdriver import Firefox
+form1 = cgi.FieldStorage() 
 
+str01 = form1.getvalue( 'TextBox1', '0' ) 
+str02 = form1.getvalue( 'TextBox2', '0' ) 
+str03 = str( int( str01 ) + int( str02 ) ) 
+# str03 = str( float( str01 ) + float( str02 ) ) 
 
-driver = Firefox()
+str0 = '''
+<html>
+  <body>
+    <form name="Form" method="POST" action="/cgi-bin/calc1.py">
+      <input type="text" size="10" name="TextBox1" value="{str11}"> + 
+      <input type="text" size="10" name="TextBox2" value="{str12}"> = 
+      <input type="text" size="10" name="TextBox3" value="{str13}"><br><br>
+      <input type="submit" value="calculate" name="button1">
+    </form>
+  </body>
+</html>
+'''.format( str11=str01, str12=str02, str13=str03 ) 
 
-
-class SeleniumMiddleware(object):
-
-    def process_request(self, request, spider):
-
-        driver.get(request.url)
-
-        return HtmlResponse(driver.current_url,
-            body = driver.page_source,
-            encoding = 'utf-8',
-            request = request)
-
-
-def close_driver():
-    driver.close()
+print("Content-Type: text/html; charset=UTF-8;\n")
+print( str0 ) 
